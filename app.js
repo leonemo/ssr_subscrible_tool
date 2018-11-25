5f3471705887b22e633eb20a3d581006d3ee98d0
andy chung <and@andydeMacBook-Pro.local>
2018-11-25 19:54:25 +0800

    Update parser code

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 /**
- * Entry File
+ * 入口文件
+ */
+
+/**
+ * 开发依赖
  */
 let path = require('path')
 let util = require('./util')
@@ -7,11 +11,17 @@ let base64 = util.base64
 let readSync = util.readSync
 let writeSync = util.writeSync
 
-let BUILD_DIR = 'dist'
-let ENTRY_FILE = './node.txt'
+let BUILD_DIR = 'dist' // 构建目录
+let ENTRY_FILE = './node.txt' // SS(R)入口
 let str = readSync(ENTRY_FILE)
 
+let checker = item => {
+    return item => item.includes('ssr://') || item.includes('ss"//')
+}
+
 // Map all the item include `ssr://` and serialize those items
-let result = str.split('\n\n').filter(item => item.includes('ssr://')).join('\r\n')
+let result = str.split('\n\n')
+                .filter(item => checker(item))
+                .join('\r\n')
 
 writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
